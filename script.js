998db888222abc5deae7428655c87bc7a3784fdd
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-06-05 11:49:03 +0100

    Shopping list items
    CSS complete

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
   databaseURL: "https://budgetshopper-d2bab-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -11,10 +11,39 @@ const shoppingListInDB= ref(datatbase, "shoppingList")
 
 const inputFieldEL = document.getElementById("input-field")
 const addButtonEL = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
+// =================================================================
+// adding to the database
 addButtonEL.addEventListener("click",function () {
   let inputValue = inputFieldEL.value
-  push(shoppingListInDB, inputValue)
-  console.log(inputValue)
-  console.log("clicked")
+
+  push(shoppingListInDB, inputValue)  // add to the DB and shopping list
+
+  clearInputFieldEl() //clear input filed after adding item to the list
+  
 })
+
+// =================================================================
+// Fetching item from the DB
+onValue(shoppingListInDB, function(snapshot){
+  let listItemsArray = Object.values(snapshot.val()) // convert DB object into array
+
+  shoppingListEl.innerContent = "" // clear old list to display updated list
+
+  for(let i =0; i < listItemsArray.length; i++){ // loop through DB to display items on the app
+    let listItem = listItemsArray[i]
+    appendItemToShoppingListEL(listItem)
+    console.log(listItem)
+  }
+})
+
+//================================================================
+// functions
+function clearInputFieldEl(){
+  inputFieldEL.value =""
+}
+
+function appendItemToShoppingListEL(itemValue){
+  shoppingListEl.innerHTML +=`<li class="list-item">${itemValue}</li>`
+}
